9f3706592807e2e3d1faa9f722c0d948ae7484fd
Cesar John L. Villanueva <cjlvillanueva@headoffice.corp.bpi.com.ph>
2025-12-11 15:57:27 +0800

    1st

---
+++ Seatwork_2.java
@@ -0,0 +1,10 @@
+package OOP_2;
+
+public class Seatwork_2 {
+	
+	public static void main(String[] args) {
+		Whiteboard board = new Whiteboard();
+		int sum1 = board.addNumbers(5, 3);
+		System.out.println("The total number of sleep is: " + String.valueOf(sum1));
+	}
+}
