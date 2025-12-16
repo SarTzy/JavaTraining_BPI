fdb10a5f0b94a336a2e8d92e35ee1ae4cb3597d0
Cesar John L. Villanueva <cjlvillanueva@headoffice.corp.bpi.com.ph>
2025-12-16 18:01:53 +0800

    updated code blocks

--- M3_Activity3.java
+++ M3_Activity3.java
@@ -31,46 +31,52 @@ public class M3_Activity3 {
 		mainMenu.forEach((key, value) -> System.out.println(key + ". " + value));
 		choice = input.nextInt();
 		switch(choice) {
-		case 1:
-			System.out.println("Enter product to search");
-			String productSearch = input.next();
+		
+			case 1:
+				System.out.println("Enter product to search");
+				String productSearch = input.next();
 			
-			if(products.containsKey(productSearch)) {
-				System.out.println("Product " + productSearch + " is found");
-			} else {
+				if(products.containsKey(productSearch)) {
+					System.out.println("Product " + productSearch + " is found");
+				} else {
 				System.out.println("Product is not found");
 			}
 			break;
-		case 2:
-			System.out.println("Add product to the list");
-			String addProduct = input.next();
-			System.out.println("Add product price to the list");
-			int addProductPrice = input.nextInt();
-			products.put(addProduct, addProductPrice);
-			System.out.println("Product " + addProduct + " has been added"); 
-			break;
-		case 3:
-			System.out.println("---Listing Productss ---");
-			products.forEach((key, value) -> System.out.println(key + ". " + value));
-			System.out.println("The total number of unique products is " + products.size());
-			System.out.println();
-			break;
-		case 4:
-			Integer leastValue = Integer.MAX_VALUE;
-			String leastKey = null;
-			for (HashMap.Entry<String, Integer> entry : products.entrySet()) {
+			
+			case 2:
+				System.out.println("Add product to the list");
+				String addProduct = input.next();
+				System.out.println("Add product price to the list");
+				int addProductPrice = input.nextInt();
+				products.put(addProduct, addProductPrice);
+				System.out.println("Product " + addProduct + " has been added"); 
+				break;
+				
+			case 3:
+				System.out.println("---Listing Productss ---");
+				products.forEach((key, value) -> System.out.println(key + ". " + value));
+				System.out.println("The total number of unique products is " + products.size());
+				System.out.println();
+				break;
+			
+			case 4:
+				Integer leastValue = Integer.MAX_VALUE;
+				String leastKey = null;
+				for (HashMap.Entry<String, Integer> entry : products.entrySet()) {
 			    if (entry.getValue() < leastValue) {
 			        leastKey = entry.getKey();
 			        leastValue = entry.getValue();
-			    }
+			    		}
+				}
+				System.out.println(leastKey + " " + leastValue);
+				break;
+				
+			case 5:
+				System.out.println("Exiting program. Goodbye!");
+				break;
 			}
-			System.out.println(leastKey + " " + leastValue);
-			break;
-		case 5:
-            System.out.println("Exiting program. Goodbye!");
-            break;
-		}
-			} while (choice != 5);
+				} while (choice != 5);
+	
 		input.close();
 	}
 }
